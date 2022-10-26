bb4f7475d21df9726fdff260b45355f031311041
jgl3hb <jgl3hb@gmail.com>
2022-10-26 13:39:13 -0400

    data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: "Blackjack",
+    subtitle: "Javascript/HTML/CSS",
+    description:
+      "A blackjack simulator built with vanilla Javascript",
+    image: "https://camo.githubusercontent.com/d95b21f623c0cde409b20d97a7899a8e86628d6e36700ac94a5ee05d3c1c8e16/68747470733a2f2f692e696d6775722e636f6d2f30734233456d342e6a7067",
+    link: "https://jgl3hb.github.io/blackjack/",
+  },
+  {
+    title: "BookNerd",
+    subtitle: "JS Front-End with MongoDB back-end",
+    description:
+      "A full CRUD app utilizing a JS frontend paired with a MongoDB backend",
+    link: "https://booknerd-1-jl.herokuapp.com/",
+  },
+  {
+    title: "WriteStuff",
+    subtitle: "Python and Flask",
+    description:
+      "A full-CRUD note-taking app built with Python and Flask",
+    link: "https://writestuff-jgl.herokuapp.com/",
+  },
+  {
+    title: "Moutain Jam",
+    subtitle: "Decoupled REACT app",
+    description:
+      "Decoupled REACT front and backend app built as a group project",
+    image: "https://camo.githubusercontent.com/b2f86d1aef389da816d09833a7d7a31c41b1453fe9d93ffe2235a5b729fd8166/68747470733a2f2f692e696d6775722e636f6d2f515a34536b6d702e706e67",
+    link: "https://mountain-jam.herokuapp.com/",
+  },
+];
