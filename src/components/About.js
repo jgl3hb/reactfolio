11f8b6f63579b11f8a304e9209cf472a60d4317a
jgl3hb <jgl3hb@gmail.com>
2022-10-26 13:46:58 -0400

    about update

---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div>
+        <div>
+          <h1>
+            Hi, I'm Jeff.
+            <br/>
+          </h1>
+          <p>
+            I'm a full-stack developer who recently graduated from General Assembly's Immersive Software Engineering Program.
+          </p>
+          <div className="flex justify-center">
+            <a>
+              Work With Me
+            </a>
+            <a>
+              See My Projects
+            </a>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+    </section>
+  );
+}
