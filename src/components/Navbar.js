34d74d6ea43a7c99caf0893e1e9a4df7ac9ac79f
jgl3hb <jgl3hb@gmail.com>
2022-10-26 13:49:00 -0400

    nav update

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header>
+      <div>
+        <a>
+          <a href="#about">
+            Jeff Lewis
+          </a>
+        </a>
+        <nav>
+          <a>
+            Past Work
+          </a>
+          <a>
+            Testimonials
+          </a>
+        </nav>
+        <a>
+          Hire Me          
+        </a>
+      </div>
+    </header>
+  );
+}
